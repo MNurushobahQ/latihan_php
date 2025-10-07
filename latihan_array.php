43d71ed721548efd5b68e07a3c865964beb36244
MNURUSHOBAHQ <e42241432@student.polije.ac.id>
2025-10-07 13:59:18 +0700

    latihan_php

---
+++ latihan_array.php
@@ -0,0 +1,14 @@
+<?php
+//pembuatan array
+$nama = array(
+1=>"Soekarno",
+2=>"Soeharto",
+3=>"gusdur",
+4=>"habibie",
+5=>"megawati");
+//cara akses array
+echo $nama[1]; //Soekarno echo "<br />";
+echo $nama[2]; //Soeharto
+echo "<br />";
+echo $nama[3]; //gusdur
+?>
